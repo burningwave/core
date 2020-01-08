57a4d7d79ff83c3307f26780f2437089c5faf0e2
Roberto <roberto.gentili.1980@gmail.com>
2020-01-08 09:44:25 +0100

    Improved code

---
+++ src/main/java/org/burningwave/core/classes/hunter/ByteCodeHunter.java
@@ -0,0 +1,88 @@
+package org.burningwave.core.classes.hunter;
+
+import java.util.function.Supplier;
+
+import org.burningwave.core.classes.ClassCriteria;
+import org.burningwave.core.classes.ClassHelper;
+import org.burningwave.core.classes.JavaClass;
+import org.burningwave.core.classes.MemberFinder;
+import org.burningwave.core.io.FileInputStream;
+import org.burningwave.core.io.FileSystemHelper;
+import org.burningwave.core.io.FileSystemHelper.Scan;
+import org.burningwave.core.io.PathHelper;
+import org.burningwave.core.io.StreamHelper;
+import org.burningwave.core.io.ZipInputStream;
+
+public class ByteCodeHunter extends ClassPathScannerWithCachingSupport<String, JavaClass, SearchContext<String, JavaClass>, ByteCodeHunter.SearchResult> {
+	
+	private ByteCodeHunter(
+		Supplier<ByteCodeHunter> byteCodeHunterSupplier,
+		Supplier<ClassHunter> classHunterSupplier,
+		FileSystemHelper fileSystemHelper,
+		PathHelper pathHelper,
+		StreamHelper streamHelper,
+		ClassHelper classHelper,
+		MemberFinder memberFinder
+	) {
+		super(
+			byteCodeHunterSupplier,
+			classHunterSupplier,
+			fileSystemHelper,
+			pathHelper,
+			streamHelper, 
+			classHelper,
+			memberFinder,
+			(initContext) -> SearchContext.<String, JavaClass>create(
+				fileSystemHelper, streamHelper, initContext
+			),
+			(context) -> new ByteCodeHunter.SearchResult(context)
+		);
+	}
+	
+	public static ByteCodeHunter create(Supplier<ByteCodeHunter> byteCodeHunterSupplier, Supplier<ClassHunter> classHunterSupplier, 
+		FileSystemHelper fileSystemHelper, PathHelper pathHelper, StreamHelper streamHelper,
+		ClassHelper classHelper, MemberFinder memberFinder
+	) {
+		return new ByteCodeHunter(byteCodeHunterSupplier, classHunterSupplier, fileSystemHelper, pathHelper, streamHelper, classHelper, memberFinder);
+	}
+	
+	@Override
+	<S extends SearchConfigAbst<S>> ClassCriteria.TestContext testCriteria(SearchContext<String, JavaClass> context, JavaClass javaClass) {
+		return context.getSearchConfig().getClassCriteria().hasNoPredicate() ?
+			context.getSearchConfig().getClassCriteria().testAndReturnTrueIfNullOrTrueByDefault(null) :
+			super.testCriteria(context, javaClass);
+	}
+	
+	@Override
+	<S extends SearchConfigAbst<S>> ClassCriteria.TestContext testCachedItem(SearchContext<String, JavaClass> context, String path, String key, JavaClass javaClass) {
+		return super.testCriteria(context, javaClass);
+	}
+	
+	@Override
+	void retrieveItemFromFileInputStream(
+		SearchContext<String, JavaClass> context, 
+		ClassCriteria.TestContext criteriaTestContext,
+		Scan.ItemContext<FileInputStream> scanItemContext,
+		JavaClass javaClass
+	) {
+		context.addItemFound(scanItemContext.getBasePathAsString(), scanItemContext.getInput().getAbsolutePath(), javaClass);
+	}
+
+	
+	@Override
+	void retrieveItemFromZipEntry(
+		SearchContext<String, JavaClass> context,
+		ClassCriteria.TestContext criteriaTestContext,
+		Scan.ItemContext<ZipInputStream.Entry> scanItemContext,
+		JavaClass javaClass) {
+		context.addItemFound(scanItemContext.getBasePathAsString(), scanItemContext.getInput().getAbsolutePath(), javaClass);
+	}
+		
+	public static class SearchResult extends org.burningwave.core.classes.hunter.SearchResult<String, JavaClass> {
+
+		public SearchResult(SearchContext<String, JavaClass> context) {
+			super(context);
+		}
+		
+	}
+}
---
+++ src/main/java/org/burningwave/core/classes/hunter/ClassHunterAbst.java
@@ -0,0 +1,207 @@
+package org.burningwave.core.classes.hunter;
+
+import java.lang.reflect.Member;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import org.burningwave.core.Criteria;
+import org.burningwave.core.classes.ClassCriteria;
+import org.burningwave.core.classes.ClassHelper;
+import org.burningwave.core.classes.JavaClass;
+import org.burningwave.core.classes.MemberCriteria;
+import org.burningwave.core.classes.MemberFinder;
+import org.burningwave.core.io.FileInputStream;
+import org.burningwave.core.io.FileSystemHelper;
+import org.burningwave.core.io.FileSystemHelper.Scan;
+import org.burningwave.core.io.PathHelper;
+import org.burningwave.core.io.StreamHelper;
+import org.burningwave.core.io.ZipInputStream;
+
+
+public abstract class ClassHunterAbst<K, R extends ClassHunterAbst.SearchResult<K>> extends ClassPathScannerWithCachingSupport<K, Class<?>, ClassHunterAbst.SearchContext<K>, R> {
+	PathMemoryClassLoader pathMemoryClassLoader;
+	
+	ClassHunterAbst(
+		Supplier<ByteCodeHunter> byteCodeHunterSupplier,
+		Supplier<ClassHunter> classHunterSupplier,
+		FileSystemHelper fileSystemHelper, 
+		PathHelper pathHelper,
+		StreamHelper streamHelper,
+		ClassHelper classHelper,
+		MemberFinder memberFinder,
+		ClassLoader parentClassLoader,
+		Function<SearchContext<K>, R> resultSupplier
+	) {
+		super(
+			byteCodeHunterSupplier,
+			classHunterSupplier,
+			fileSystemHelper,
+			pathHelper,
+			streamHelper,
+			classHelper,
+			memberFinder,
+			(variableInitObjects) -> ClassHunterAbst.SearchContext._create(
+				fileSystemHelper, streamHelper, variableInitObjects
+			),
+			resultSupplier
+		);
+		this.pathMemoryClassLoader = PathMemoryClassLoader.create(
+			parentClassLoader, pathHelper, classHelper, byteCodeHunterSupplier
+		);
+	}
+	
+	@Override
+	public R findBy(ClassFileScanConfig scanConfig, SearchConfig searchConfig) {
+		searchConfig.getClassCriteria().collectMembers(true);
+		return super.findBy(scanConfig, searchConfig);
+	}
+	
+	@Override
+	public R findBy(CacheableSearchConfig searchConfig) {
+		searchConfig.getClassCriteria().collectMembers(true);
+		return super.findBy(searchConfig);
+	}
+	
+	@Override
+	<S extends SearchConfigAbst<S>> ClassCriteria.TestContext testCachedItem(ClassHunterAbst.SearchContext<K> context, String path, K key, Class<?> cls) {
+		return context.testCriteria(cls);
+	}
+	
+	@Override
+	<S extends SearchConfigAbst<S>> void addCachedItemToContext(
+		ClassHunterAbst.SearchContext<K> context, ClassCriteria.TestContext testContext, String path, Entry<K, Class<?>> cachedItemAsEntry
+	) {
+		context.addItemFound(path, cachedItemAsEntry.getKey(), cachedItemAsEntry.getValue(), testContext.getMembersFound());
+	}
+	
+	@Override
+	void retrieveItemFromFileInputStream(
+		ClassHunterAbst.SearchContext<K> context, 
+		ClassCriteria.TestContext criteriaTestContext,
+		Scan.ItemContext<FileInputStream> scanItemContext, 
+		JavaClass javaClass
+	) {
+		context.addItemFound(
+			scanItemContext.getBasePathAsString(),
+			buildKey(scanItemContext.getInput().getAbsolutePath()),
+			criteriaTestContext.getEntity(),
+			criteriaTestContext.getMembersFound()
+		);
+	}
+	
+	abstract K buildKey(String absolutePath) ;
+	
+	@Override
+	void retrieveItemFromZipEntry(ClassHunterAbst.SearchContext<K> context, ClassCriteria.TestContext criteriaTestContext, Scan.ItemContext<ZipInputStream.Entry> scanItemContext, JavaClass javaClass) {
+		context.addItemFound(
+			scanItemContext.getBasePathAsString(),
+			buildKey(scanItemContext.getInput().getAbsolutePath()),
+			criteriaTestContext.getEntity(),
+			criteriaTestContext.getMembersFound()
+		);
+	}
+	
+	
+	@Override
+	public void close() {
+		pathMemoryClassLoader.close();
+		pathMemoryClassLoader = null;
+		super.close();
+	}
+	
+	public static class SearchContext<K> extends org.burningwave.core.classes.hunter.SearchContext<K, Class<?>> {
+		Map<Class<?>, Map<MemberCriteria<?, ?, ?>, Collection<Member>>> membersFound;
+		private Map<MemberCriteria<?, ?, ?>, Collection<Member>> membersFoundFlatMap;
+		
+		static <K> SearchContext<K> _create(FileSystemHelper fileSystemHelper, StreamHelper streamHelper, InitContext initContext) {
+			return new SearchContext<K>(fileSystemHelper, streamHelper,  initContext);
+		}
+		
+		SearchContext(FileSystemHelper fileSystemHelper, StreamHelper streamHelper, InitContext initContext) {
+			super(fileSystemHelper, streamHelper, initContext);
+			membersFound = new ConcurrentHashMap<>();
+			membersFoundFlatMap = new ConcurrentHashMap<>();
+		}
+		
+		void addItemFound(String path, K key, Class<?> item, Map<MemberCriteria<?, ?, ?>, Collection<Member>> membersForCriteria) {
+			super.addItemFound(path, key, item);
+			try {
+				this.membersFound.put(item, membersForCriteria);
+			} catch (NullPointerException exc) {
+				// TODO Auto-generated catch block
+				exc.printStackTrace();
+			}
+			membersForCriteria.forEach((criteria, memberList) -> {
+				Collection<Member> coll = membersFoundFlatMap.get(criteria);
+				if (coll == null) {								
+					coll = new CopyOnWriteArrayList<>();
+					membersFoundFlatMap.put(criteria, coll);
+				}
+				coll.addAll(memberList);
+			});	
+		}
+		
+		void addAllMembersFound(Class<?> cls, Map<MemberCriteria<?, ?, ?>, Collection<Member>> membersFound) {
+			this.membersFound.put(cls, membersFound);
+			this.membersFoundFlatMap.putAll(membersFound);
+		}
+		
+		Map<Class<?>, Map<MemberCriteria<?, ?, ?>, Collection<Member>>> getMembersFound() {
+			return membersFound;
+		}
+		
+		public Map<MemberCriteria<?, ?, ?>, Collection<Member>> getMembersFoundFlatMap() {
+			return membersFoundFlatMap;
+		}
+		
+		@Override
+		public void close() {
+			membersFound.clear();
+			membersFound = null;
+			membersFoundFlatMap.clear();
+			membersFoundFlatMap = null;
+			super.close();
+		}
+	}
+
+	public static class SearchResult<K> extends org.burningwave.core.classes.hunter.SearchResult<K, Class<?>> {
+		SearchResult(SearchContext<K> context) {
+			super(context);
+		}
+		
+		public Map<Class<?>, Map<MemberCriteria<?, ?, ?>, Collection<Member>>> getMembersFound() {
+			return ((SearchContext<K>)this.context).getMembersFound();
+		}
+		
+		public Map<MemberCriteria<?, ?, ?>, Collection<Member>> getMembersFoundFlatMap() {
+			return ((SearchContext<K>)this.context).getMembersFoundFlatMap();
+		}
+		
+		@SuppressWarnings("unchecked")
+		public <M extends Member, C extends MemberCriteria<M, C, T>, T extends Criteria.TestContext<M, C>> Collection<Member> getMembersFoundBy(C criteria) {
+			Collection<Member> membersFoundByCriteria = getMembersFoundFlatMap().get(criteria);
+			if (membersFoundByCriteria != null && membersFoundByCriteria.size() > 0) {
+				return membersFoundByCriteria;
+			} else {
+				C criteriaCopy = criteria.createCopy();
+				criteriaCopy.init(context.getSearchConfig().getClassCriteria().getClassSupplier(), context.getSearchConfig().getClassCriteria().getByteCodeSupplier());
+				criteriaCopy.useClasses(context.getSearchConfig().getClassCriteria().getClassesToBeUploaded());
+				final Collection<Member> membersFoundByCriteriaFinal = new CopyOnWriteArrayList<>();
+				((SearchContext<K>)this.context).getMembersFoundFlatMap().values().forEach((membersCollection) -> {
+					membersCollection.stream().filter(
+						(member) -> criteriaCopy.testAndReturnFalseIfNullOrTrueByDefault((M)member).getResult()
+					).collect(
+						Collectors.toCollection(() -> membersFoundByCriteriaFinal)
+					);
+				});
+				return membersFoundByCriteriaFinal;
+			}
+		}
+	}
+}
