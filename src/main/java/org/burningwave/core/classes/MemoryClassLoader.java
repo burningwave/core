deb32db7d94c7d756c4dd8e301e44b3c5ba026f5
Roberto <roberto.gentili.1980@gmail.com>
2020-02-05 15:53:15 +0100

    Improved performance

--- src/main/java/org/burningwave/core/classes/MemoryClassLoader.java
+++ src/main/java/org/burningwave/core/classes/MemoryClassLoader.java
@@ -83,7 +83,7 @@ public class MemoryClassLoader extends ClassLoader implements Component {
 	}
 
 	public void addCompiledClass(String className, ByteBuffer byteCode) {
-		synchronized (this.toString() + "_" + className) {
+		synchronized (notLoadedCompiledClasses.toString() + "_" + className) {
 	    	if (classHelper.retrieveLoadedClass(this, className) == null) {
 	    		notLoadedCompiledClasses.put(className, byteCode);
 			} else {
@@ -221,7 +221,7 @@ public class MemoryClassLoader extends ClassLoader implements Component {
     
 	@Override
     protected Class<?> findClass(String className) throws ClassNotFoundException {
-		synchronized (this.toString() + "_" + className) {
+		synchronized (notLoadedCompiledClasses.toString() + "_" + className) {
 			Class<?> cls = null;
 			ByteBuffer byteCode = notLoadedCompiledClasses.get(className);
 			if (byteCode != null) {
@@ -280,7 +280,7 @@ public class MemoryClassLoader extends ClassLoader implements Component {
 	 }
 
 	public void removeNotLoadedCompiledClass(String className) {
-		synchronized (this.toString() + "_" + className) {
+		synchronized (notLoadedCompiledClasses.toString() + "_" + className) {
 			notLoadedCompiledClasses.remove(className);
 		}
 	}
