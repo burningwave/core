f808183cd11cb09783295ae4b4fd82cd643d3ed5
Roberto <roberto.gentili.1980@gmail.com>
2020-02-19 18:43:14 +0100

    Improved code

--- src/main/resources/compileClassLoaderDelegate.cmd
+++ src/main/resources/compileClassLoaderDelegate.cmd
@@ -1,5 +1,5 @@
 @echo off
-del %~dp0jdk\internal\loader\ClassLoaderDelegate.bwclass
+del %~dp0org\burningwave\core\classes\ClassLoaderDelegate.bwclass
 F:\Shared\Programmi\Java\jdk\9.0.1\bin\javac.exe -cp "%~dp0..\..\..\target\classes;%~dp0;" %~dp0jdk\internal\loader\ClassLoaderDelegate.java
-ren %~dp0jdk\internal\loader\ClassLoaderDelegate.class ClassLoaderDelegate.bwclass
+move %~dp0jdk\internal\loader\ClassLoaderDelegate.class %~dp0org\burningwave\core\classes\ClassLoaderDelegate.bwclass
 pause
