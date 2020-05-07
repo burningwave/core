56a76b40efe0dc5c6227ab18f6837438c6e268c8
Roberto <roberto.gentili.1980@gmail.com>
2020-05-07 10:41:32 +0200

    Added comment

--- src/test/java/org/burningwave/core/examples/usecase006/Finder.java
+++ src/test/java/org/burningwave/core/examples/usecase006/Finder.java
@@ -12,21 +12,31 @@ import org.burningwave.core.io.PathHelper;
 
 public class Finder {
 
-	public Collection<Class<?>> find() {
-		ComponentSupplier componentSupplier = ComponentContainer.getInstance();
-		PathHelper pathHelper = componentSupplier.getPathHelper();
-		ClassHunter classHunter = componentSupplier.getClassHunter();
+    public Collection<Class<?>> find() {
+        ComponentSupplier componentSupplier = ComponentContainer.getInstance();
+        PathHelper pathHelper = componentSupplier.getPathHelper();
+        ClassHunter classHunter = componentSupplier.getClassHunter();
 
-		CacheableSearchConfig searchConfig = SearchConfig.forPaths(
-			//Here you can add all absolute path you want:
-			//both folders, zip and jar will be recursively scanned.
-			//For example you can add: "C:\\Users\\user\\.m2"
-			//With the row below the search will be executed on runtime Classpaths
-			pathHelper.getMainClassPaths()
-		);
-
-		SearchResult searchResult = classHunter.findBy(searchConfig);
-		return searchResult.getClasses();
-	}
+        CacheableSearchConfig searchConfig = SearchConfig.forPaths(
+            //Here you can add all absolute path you want:
+            //both folders, zip, jar, war, ear and jmod will be recursively scanned.
+            //For example you can add: "C:\\Users\\user\\.m2"
+            //With the row below the search will be executed on runtime Classpaths
+            pathHelper.getMainClassPaths()
+        );
+        
+        //The loadInCache method loads all classes in the paths of the SearchConfig received as input
+        //and then execute the queries of the ClassCriteria on the cache data. Once the data has been 
+        //cached, it is possible to take advantage of faster searches for the loaded paths also through 
+        //the findBy method. In addition to the loadCache method, loading data into the cache can also
+        //take place via the findBy method if the latter receives a SearchConfig without ClassCriteria
+        //as input. It is possible to clear the cache individually for all hunters (ClassHunter, 
+        //ByteCodeHunter and ClassPathHunter) but to avoid inconsistencies it is recommended to perform
+        //this cleaning using the clearHuntersCache method of the ComponentSupplier.
+        //To perform searches that do not use the cache use method findBy(ClassFileScanConfig, SearchConfig) 
+        SearchResult searchResult = classHunter.loadInCache(searchConfig).find();
+        
+        return searchResult.getClasses();
+    }
 
 }
