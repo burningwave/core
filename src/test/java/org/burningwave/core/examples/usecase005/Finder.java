4d27277effb1dea0266506980a7cf1c9329c7ebd
Roberto <roberto.gentili.1980@gmail.com>
2020-05-07 11:19:15 +0200

    Improved indentation

--- src/test/java/org/burningwave/core/examples/usecase005/Finder.java
+++ src/test/java/org/burningwave/core/examples/usecase005/Finder.java
@@ -14,31 +14,31 @@ import org.burningwave.core.io.PathHelper;
 
 public class Finder {
 
-	public Collection<Class<?>> find() {
-		ComponentSupplier componentSupplier = ComponentContainer.getInstance();
-		PathHelper pathHelper = componentSupplier.getPathHelper();
-		ClassHunter classHunter = componentSupplier.getClassHunter();
+    public Collection<Class<?>> find() {
+        ComponentSupplier componentSupplier = ComponentContainer.getInstance();
+        PathHelper pathHelper = componentSupplier.getPathHelper();
+        ClassHunter classHunter = componentSupplier.getClassHunter();
 
-		CacheableSearchConfig searchConfig = SearchConfig.forPaths(
-			//Here you can add all absolute path you want:
-			//both folders, zip and jar will be recursively scanned.
-			//For example you can add: "C:\\Users\\user\\.m2"
-			//With the row below the search will be executed on runtime Classpaths
-			pathHelper.getMainClassPaths()
-		).by(
-			ClassCriteria.create().byClasses((uploadedClasses, currentScannedClass) ->
-				//[1]here you recall the uploaded class by "useClasses" method. In this case we find all class who extends java.util.AbstractList
-				uploadedClasses.get(AbstractList.class).isAssignableFrom(currentScannedClass)
-			).useClasses(
-				//With this directive we ask the library to load one or more classes to be used for comparisons:
-				//it serves to eliminate the problem that a class, loaded by different class loaders, 
-				//turns out to be different for the comparison operators (eg. The isAssignableFrom method).
-				//If you call this method, you must retrieve the uploaded class in all methods that support this feature like in the point[1]
-				AbstractList.class
-			)
-		);
+        CacheableSearchConfig searchConfig = SearchConfig.forPaths(
+            //Here you can add all absolute path you want:
+            //both folders, zip and jar will be recursively scanned.
+            //For example you can add: "C:\\Users\\user\\.m2"
+            //With the row below the search will be executed on runtime Classpaths
+            pathHelper.getMainClassPaths()
+        ).by(
+            ClassCriteria.create().byClasses((uploadedClasses, currentScannedClass) ->
+                //[1]here you recall the uploaded class by "useClasses" method. In this case we find all class who extends java.util.AbstractList
+                uploadedClasses.get(AbstractList.class).isAssignableFrom(currentScannedClass)
+            ).useClasses(
+                //With this directive we ask the library to load one or more classes to be used for comparisons:
+                //it serves to eliminate the problem that a class, loaded by different class loaders, 
+                //turns out to be different for the comparison operators (eg. The isAssignableFrom method).
+                //If you call this method, you must retrieve the uploaded class in all methods that support this feature like in the point[1]
+                AbstractList.class
+            )
+        );
         
-		//The loadInCache method loads all classes in the paths of the SearchConfig received as input
+        //The loadInCache method loads all classes in the paths of the SearchConfig received as input
         //and then execute the queries of the ClassCriteria on the cached data. Once the data has been 
         //cached, it is possible to take advantage of faster searches for the loaded paths also through 
         //the findBy method. In addition to the loadCache method, loading data into the cache can also
@@ -47,9 +47,9 @@ public class Finder {
         //ByteCodeHunter and ClassPathHunter) but to avoid inconsistencies it is recommended to perform
         //this cleaning using the clearHuntersCache method of the ComponentSupplier.
         //To perform searches that do not use the cache use method findBy(ClassFileScanConfig, SearchConfig)
-		SearchResult searchResult = classHunter.loadInCache(searchConfig).find();
-		
-		return searchResult.getClasses();
-	}
+        SearchResult searchResult = classHunter.loadInCache(searchConfig).find();
+        
+        return searchResult.getClasses();
+    }
 
 }
