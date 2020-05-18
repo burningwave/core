d101e56a71ed603976716872842d77c5b8725f9c
Roberto <roberto.gentili.1980@gmail.com>
2020-05-18 11:13:21 +0200

    Added examples

---
+++ src/test/java/org/burningwave/core/examples/filesystemscanner/ClassFileScannerBasedOnFileName.java
@@ -0,0 +1,69 @@
+package org.burningwave.core.examples.filesystemscanner;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.function.Predicate;
+
+import org.burningwave.core.assembler.ComponentContainer;
+import org.burningwave.core.assembler.ComponentSupplier;
+import org.burningwave.core.classes.JavaClass;
+import org.burningwave.core.io.FileScanConfig;
+import org.burningwave.core.io.FileScanConfigAbst;
+import org.burningwave.core.io.FileSystemItem;
+import org.burningwave.core.io.IterableZipContainer.Entry;
+
+public class ClassFileScannerBasedOnFileName {
+    
+    public static void execute() {
+        ComponentSupplier componentSupplier = ComponentContainer.getInstance();
+        Collection<FileSystemItem> files = new HashSet<>();
+        componentSupplier.getFileSystemScanner().scan(
+            new FileScanConfig() {
+                @Override
+                protected Predicate<File> getFileNameCheckerForFileSystemEntry() {
+                    return entry -> {
+                        String name = entry.getName();
+                        return name.endsWith(".class") && 
+                            !name.endsWith("module-info.class") &&
+                            !name.endsWith("package-info.class");
+                    };
+                }
+                
+                @Override
+                protected Predicate<Entry> getFileNameCheckerForZipEntry() {
+                    return entry -> {
+                        String name = entry.getName();
+                        return name.endsWith(".class") && 
+                            !name.endsWith("module-info.class") &&
+                            !name.endsWith("package-info.class");
+                    };
+                }
+                
+            }.checkFileOptions(
+                FileScanConfigAbst.CHECK_FILE_NAME
+            ).addPaths(
+                componentSupplier.getPathHelper().getMainClassPaths()
+            ).toScanConfiguration(
+                (wrapper) -> {
+                    FileSystemItem fIS = wrapper.getScannedItem().toFileSystemItem();
+                    System.out.println(fIS.getAbsolutePath());
+                    files.add(fIS);
+                    JavaClass.create(
+                        wrapper.getScannedItem().toByteBuffer()
+                    ).storeToClassPath(
+                        System.getProperty("user.home") + "/Desktop/bw-tests"
+                    );
+                }
+            )
+        );
+        System.out.println("Files found: " + files.size());
+    }
+    
+    
+    
+    public static void main(String[] args) throws Throwable {
+        execute();
+    }
+    
+}
