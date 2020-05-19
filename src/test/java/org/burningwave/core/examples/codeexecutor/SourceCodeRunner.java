570a53a5090f6308aae73e971668e7567b5bb687
Roberto <roberto.gentili.1980@gmail.com>
2020-05-19 17:37:51 +0200

    Released new version

---
+++ src/test/java/org/burningwave/core/examples/codeexecutor/SourceCodeRunner.java
@@ -0,0 +1,34 @@
+package org.burningwave.core.examples.codeexecutor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.burningwave.core.assembler.ComponentContainer;
+import org.burningwave.core.assembler.ComponentSupplier;
+import org.burningwave.core.classes.CodeExecutor;
+import org.burningwave.core.classes.StatementSourceGenerator;
+
+public class SourceCodeRunner {
+    
+    public static Integer execute() {
+        ComponentSupplier componentSupplier = ComponentContainer.getInstance();
+        return componentSupplier.getClassFactory().execute(
+            CodeExecutor.Config.forStatementSourceGenerator(
+                StatementSourceGenerator.createSimple().setElementPrefix("\t")
+                .useType(ArrayList.class, List.class)
+                .addCodeRow("System.out.println(\"number to add: \" + parameter[0]);")
+                .addCodeRow("List<Integer> numbers = new ArrayList<>();")
+                .addCodeRow("numbers.add((Integer)parameter[0]);")
+                .addCodeRow("System.out.println(\"number list size: \" + numbers.size());")
+                .addCodeRow("System.out.println(\"number in the list: \" + numbers.get(0));")
+                .addCodeRow("Integer inputNumber = (Integer)parameter[0];")
+                .addCodeRow("return (T)new Integer(inputNumber + (Integer)parameter[1]);")        
+            ).withParameter(Integer.valueOf(5), Integer.valueOf(3))
+        );
+        
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Total is: " + execute());
+    }
+}
