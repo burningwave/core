2d343feafa488fcdb4001304c1e8180328735558
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-05-31 22:15:02 +0200

    Added test

--- src/test/java/org/burningwave/core/MemoryClassLoaderTest.java
+++ src/test/java/org/burningwave/core/MemoryClassLoaderTest.java
@@ -2,7 +2,16 @@ package org.burningwave.core;
 
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+import org.burningwave.core.assembler.ComponentSupplier;
+import org.burningwave.core.assembler.StaticComponentContainer;
+import org.burningwave.core.classes.ClassSourceGenerator;
+import org.burningwave.core.classes.JavaMemoryCompiler;
 import org.burningwave.core.classes.MemoryClassLoader;
+import org.burningwave.core.classes.TypeDeclarationSourceGenerator;
+import org.burningwave.core.classes.UnitSourceGenerator;
 import org.burningwave.core.reflection.PropertyAccessor;
 import org.junit.jupiter.api.Test;
 
@@ -35,6 +44,31 @@ public class MemoryClassLoaderTest extends BaseTest {
 		MemoryClassLoader memoryClassLoader = getMemoryClassLoader(null);
 		memoryClassLoader.loadOrUploadClass(PropertyAccessor.class);
 		assertTrue(memoryClassLoader.hasPackageBeenDefined(PropertyAccessor.class.getPackage().getName()));
+	}
+	
+	@Test
+	public void loadClassTestTwo() throws ClassNotFoundException {
+		testNotNull(() -> {
+			ComponentSupplier componentSupplier = getComponentSupplier();
+			ClassSourceGenerator ClassSG = ClassSourceGenerator.create(
+				TypeDeclarationSourceGenerator.create("ReTry")
+			).addModifier(
+				Modifier.PUBLIC
+			).addInnerClass(
+				ClassSourceGenerator.create(
+					TypeDeclarationSourceGenerator.create("ReReTry")
+				).addModifier(
+					Modifier.PUBLIC | Modifier.STATIC
+				).expands(TypeDeclarationSourceGenerator.create("ReTry"))
+			);
+			UnitSourceGenerator unitSG = UnitSourceGenerator.create("tryyy").addClass(
+				ClassSG
+			).addStaticImport(StaticComponentContainer.class, "Streams", "Classes");
+			JavaMemoryCompiler jMC = componentSupplier.getJavaMemoryCompiler();
+			MemoryClassLoader memoryClassLoader = getMemoryClassLoader(null);
+			memoryClassLoader.addByteCodes(jMC.compile(Arrays.asList(unitSG.make())).entrySet());
+			return memoryClassLoader.loadClass("tryyy.ReTry$ReReTry");
+		});
 	}
 	
 }
