2668c179ac792d9df7db16a3afd97b56eb8e387f
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-06-01 19:20:19 +0200

    Improved code and Releasing new version

--- src/main/java/org/burningwave/core/classes/ClassPathScannerAbst.java
+++ src/main/java/org/burningwave/core/classes/ClassPathScannerAbst.java
@@ -61,7 +61,7 @@ public abstract class ClassPathScannerAbst<I, C extends SearchContext<I>, R exte
 
 			DEFAULT_VALUES.put(
 				Key.DEFAULT_SEARCH_CONFIG_PATHS, 
-				PathHelper.Configuration.Key.MAIN_CLASS_PATHS_PLACE_HOLDER
+				PathHelper.Configuration.Key.MAIN_CLASS_PATHS_PLACE_HOLDER + ";"
 			);
 		}
 	}
