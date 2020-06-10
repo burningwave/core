19138a5350d7db56a6286b01e42e77571ce1ab84
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-06-10 23:13:52 +0200

    Readded tests

--- src/test/java/org/burningwave/core/AllExceptHeavyTestsSuite.java
+++ src/test/java/org/burningwave/core/AllExceptHeavyTestsSuite.java
@@ -2,16 +2,15 @@ package org.burningwave.core;
 
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.ExcludeTags;
-import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-//@SelectPackages("org.burningwave.core")
-@SelectClasses({
-	//ClassHunterTest.class, 
-	FileSystemItemTest.class
-})
+@SelectPackages("org.burningwave.core")
+//@SelectClasses({
+//	//ClassHunterTest.class, 
+//	FileSystemItemTest.class
+//})
 @ExcludeTags("Heavy")
 public class AllExceptHeavyTestsSuite {
 
