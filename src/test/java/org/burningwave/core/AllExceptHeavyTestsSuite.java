b92c353612eb21ac8f03b8443196ffc10ef03190
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-06-10 22:15:43 +0200

    Restricted tests

--- src/test/java/org/burningwave/core/AllExceptHeavyTestsSuite.java
+++ src/test/java/org/burningwave/core/AllExceptHeavyTestsSuite.java
@@ -2,11 +2,13 @@ package org.burningwave.core;
 
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.ExcludeTags;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-@SelectPackages("org.burningwave.core")
+//@SelectPackages("org.burningwave.core")
+@SelectClasses(FileSystemItemTest.class)
 @ExcludeTags("Heavy")
 public class AllExceptHeavyTestsSuite {
 
