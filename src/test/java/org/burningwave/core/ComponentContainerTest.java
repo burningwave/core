654b2c30962b63b82c0ed7674b5a7f7a03cb456a
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-07-01 22:12:23 +0200

    Changed tests workflow

--- src/test/java/org/burningwave/core/ComponentContainerTest.java
+++ src/test/java/org/burningwave/core/ComponentContainerTest.java
@@ -28,4 +28,12 @@ public class ComponentContainerTest extends BaseTest {
 		});
 	}
 	
+	@Test
+	public void clearAll() {
+		logWarn("Total memory before clearAll {}", Runtime.getRuntime().totalMemory());
+		ComponentContainer.clearAll();
+		System.gc();
+		logWarn("Total memory after clearAll {}", Runtime.getRuntime().totalMemory());
+	}
+	
 }
--- src/test/java/org/burningwave/core/RepeatedClassFactoryTest.java
+++ src/test/java/org/burningwave/core/RepeatedClassFactoryTest.java
@@ -1,19 +1,6 @@
 package org.burningwave.core;
 
-import org.burningwave.core.assembler.ComponentContainer;
-import org.junit.jupiter.api.Test;
-
 public class RepeatedClassFactoryTest extends ClassFactoryTest {
-	
-	@Test
-	public void getOrBuildClassWithExternalClassLastTest() {
-		logWarn("Total memory before clearAll {}", Runtime.getRuntime().totalMemory());
-		ComponentContainer.clearAll();
-		System.gc();
-		logWarn("Total memory after clearAll {}", Runtime.getRuntime().totalMemory());
-		testDoesNotThrow(() -> {
-			getOrBuildClassWithExternalClassTestOne(true, null);
-		});
-	}
+
 	
 }
