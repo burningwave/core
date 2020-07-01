110cfcc3ee6d6f58e94c2004b3945004e324dc37
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-07-01 13:38:38 +0200

    Added cleaning operations

--- src/test/java/org/burningwave/core/RepeatedClassFactoryTest.java
+++ src/test/java/org/burningwave/core/RepeatedClassFactoryTest.java
@@ -1,5 +1,19 @@
 package org.burningwave.core;
 
-public class RepeatedClassFactoryTest extends ClassFactoryTest {
+import org.burningwave.core.assembler.ComponentContainer;
+import org.junit.jupiter.api.Test;
 
+public class RepeatedClassFactoryTest extends ClassFactoryTest {
+	
+	@Test
+	public void getOrBuildClassWithExternalClassLastTest() {
+		logWarn("Total memory before clearAll {}", Runtime.getRuntime().totalMemory());
+		ComponentContainer.clearAll();
+		System.gc();
+		logWarn("Total memory after clearAll {}", Runtime.getRuntime().totalMemory());
+		testDoesNotThrow(() -> {
+			getOrBuildClassWithExternalClassTestOne(true, null);
+		});
+	}
+	
 }
