684a377ec7cb09e84b27859c0207a7dae0e5f326
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-07-05 15:27:18 +0200

    Improved functionality and Releasing new version

--- src/test/java/org/burningwave/core/FieldsTest.java
+++ src/test/java/org/burningwave/core/FieldsTest.java
@@ -2,11 +2,16 @@ package org.burningwave.core;
 
 
 import static org.burningwave.core.assembler.StaticComponentContainer.Fields;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.burningwave.core.assembler.ComponentContainer;
 import org.junit.jupiter.api.Test;
 
 
+@SuppressWarnings("unused")
 public class FieldsTest extends BaseTest {
 	
 	@Test
@@ -25,5 +30,38 @@ public class FieldsTest extends BaseTest {
 				return Fields.getAllDirect(ComponentContainer.getInstance()).values();	
 			},
 		true);
+	}
+	
+	@Test
+	public void setDirectTestOne() {
+		testDoesNotThrow(() -> {
+			Object obj = new Object() {
+				List<Object> objectValue;
+				int intValue;
+				long longValue;
+				float floatValue;
+				double doubleValue;
+				boolean booleanValue;
+			};
+			List<Object> objectValue = new ArrayList<>();
+			Fields.setDirect(obj, "objectValue", objectValue);
+			List<Object> objectValue2Var = Fields.getDirect(obj, "objectValue");
+			assertTrue(objectValue2Var == objectValue);
+			Fields.setDirect(obj, "intValue", 1);
+			int intValue = Fields.getDirect(obj, "intValue");
+			assertTrue(intValue == 1);
+			Fields.setDirect(obj, "longValue", 2L);
+			long longValue = Fields.getDirect(obj, "longValue");
+			assertTrue(longValue == 2l);
+			Fields.setDirect(obj, "floatValue", 3f);
+			float floatValue = Fields.getDirect(obj, "floatValue");
+			assertTrue(floatValue == 3f);
+			Fields.setDirect(obj, "doubleValue", 4.0);
+			double doubleValue = Fields.getDirect(obj, "doubleValue");
+			assertTrue(doubleValue == 4.0);
+			Fields.setDirect(obj, "booleanValue", true);
+			boolean booleanValue = Fields.getDirect(obj, "booleanValue");
+			assertTrue(booleanValue);
+		});
 	}
 }
