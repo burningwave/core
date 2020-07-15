c41e3af2de21e65bb29d102504999a1a6eaddb22
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-07-15 17:18:56 +0200

    Added test

--- src/test/java/org/burningwave/core/AllExceptHeavyTestsSuite.java
+++ src/test/java/org/burningwave/core/AllExceptHeavyTestsSuite.java
@@ -33,6 +33,7 @@ import org.junit.runner.RunWith;
     StringsTest.class,
     UnitSourceGeneratorTest.class,
     RepeatedClassFactoryTest.class,
+    FieldsTest.class,
     ComponentContainerTest.class
 })
 @ExcludeTags("Heavy")
