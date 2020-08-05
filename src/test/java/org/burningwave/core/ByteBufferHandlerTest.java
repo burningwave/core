e49e4d3825452d7bb8928e3e303c894585e03454
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-08-05 21:59:17 +0200

    Added test

--- src/test/java/org/burningwave/core/ByteBufferHandlerTest.java
+++ src/test/java/org/burningwave/core/ByteBufferHandlerTest.java
@@ -1,6 +1,8 @@
 package org.burningwave.core;
 
 import static org.burningwave.core.assembler.StaticComponentContainer.ByteBufferHandler;
+
+import org.burningwave.core.jvm.LowLevelObjectsHandler.ByteBufferHandler.Deallocator;
 import org.junit.jupiter.api.Test;
 
 public class ByteBufferHandlerTest extends BaseTest {
@@ -11,6 +13,16 @@ public class ByteBufferHandlerTest extends BaseTest {
 		testNotNull(() -> {
 			Long.valueOf(ByteBufferHandler.getAddress(ByteBufferHandler.allocateDirect(1)));
 			return Long.valueOf(ByteBufferHandler.getAddress(ByteBufferHandler.allocateDirect(1)));
+		});
+	}
+	
+	@Test
+	public void getDeallocatorTest() {
+		testDoesNotThrow(() -> {
+			Deallocator deallocator = ByteBufferHandler.getDeallocator(ByteBufferHandler.allocateDirect(1));
+			deallocator.freeMemory();
+			deallocator.freeMemory();
+			ByteBufferHandler.getDeallocator(ByteBufferHandler.allocateDirect(1)).freeMemory();
 		});
 	}
 	
