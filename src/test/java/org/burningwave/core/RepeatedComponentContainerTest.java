f422ba46325c9df57db8eae3547b2a149d15003a
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-08-06 14:36:51 +0200

    Added tests

--- src/test/java/org/burningwave/core/RepeatedComponentContainerTest.java
+++ src/test/java/org/burningwave/core/RepeatedComponentContainerTest.java
@@ -1,50 +1,5 @@
 package org.burningwave.core;
 
-import static org.burningwave.core.assembler.StaticComponentContainer.BackgroundExecutor;
-import static org.burningwave.core.assembler.StaticComponentContainer.GlobalProperties;
-
-import org.burningwave.core.assembler.ComponentContainer;
-import org.junit.jupiter.api.Test;
-
-public class RepeatedComponentContainerTest extends BaseTest {
-
+public class RepeatedComponentContainerTest extends ComponentContainerTest {
 	
-	@Test
-	public void reInitAndCloseTest() {
-		testDoesNotThrow(() -> {
-			ComponentContainer componentSupplier = ComponentContainer.create("burningwave.properties");
-			componentSupplier.getClassFactory();
-			componentSupplier.getClassHunter();
-			componentSupplier.getClassPathHunter();
-			componentSupplier.getCodeExecutor();
-			componentSupplier.getPathHelper();
-			GlobalProperties.put("newPropertyName", "newPropertyValue");
-			componentSupplier.reInit();
-			componentSupplier.getClassFactory();
-			componentSupplier.getClassHunter();
-			componentSupplier.getClassPathHunter();
-			componentSupplier.getCodeExecutor();
-			componentSupplier.getPathHelper();
-			componentSupplier.close();
-		});
-	}
-	
-	@Test
-	public void clearAll() {
-		testDoesNotThrow(() -> {
-			logWarn("Total memory before clearAll {}", Runtime.getRuntime().totalMemory());
-			ComponentContainer.clearAll();
-			BackgroundExecutor.waitForExecutablesEnding();
-			System.gc();
-			logWarn("Total memory after clearAll {}", Runtime.getRuntime().totalMemory());
-		});
-	}
-	
-	
-	@Test
-	public void reInit() {
-		testDoesNotThrow(() -> {
-			getComponentSupplier().reInit();
-		});
-	}
 }
