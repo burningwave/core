781899c7e82dfa9a812f38a59a6be77b455db64c
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-09-11 15:08:29 +0200

    Removed tests

--- src/test/java/org/burningwave/core/AllExceptHeavyTestsSuite.java
+++ src/test/java/org/burningwave/core/AllExceptHeavyTestsSuite.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
 	ByteCodeHunterTest.class,
     ClassesTest.class,
     ClassFactoryTest.class,
-    ComponentContainerTest.class,
+    //ComponentContainerTest.class,
     ClassHunterTest.class,
     ClassLoadersTest.class,
     ClassPathHunterTest.class,
@@ -35,7 +35,7 @@ import org.junit.runner.RunWith;
     StringsTest.class,
     UnitSourceGeneratorTest.class,
     RepeatedClassFactoryTest.class,
-    RepeatedComponentContainerTest.class 
+    //RepeatedComponentContainerTest.class 
 })
 @ExcludeTags("Heavy")
 public class AllExceptHeavyTestsSuite {
