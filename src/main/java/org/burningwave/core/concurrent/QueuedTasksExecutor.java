d1961d7ac057a4e89eb65af5d7d0cb7cb91d9a67
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-09-14 12:56:03 +0200

    Trying

--- src/main/java/org/burningwave/core/concurrent/QueuedTasksExecutor.java
+++ src/main/java/org/burningwave/core/concurrent/QueuedTasksExecutor.java
@@ -915,13 +915,13 @@ public class QueuedTasksExecutor implements Component {
 							synchronized(getMutex("executingFinishedWaiter")) {
 								if (!tasksQueue.isEmpty()) {
 									try {
-										if (taskCreationTrackingEnabled && currentTask.getCreatorInfos() != null) {
-											logInfo("Sleeping for 1 second while executing {}:{}, current task queue size: {}\n\t{}", this.currentTask.executable, this.currentTask.started, tasksQueue.size(),
-													String.join("\n\t", this.currentTask.getCreatorInfos().stream().map(st -> st.toString()).collect(Collectors.toList())));
-											Thread.sleep(1000);
-										} else {
+//										if (taskCreationTrackingEnabled && currentTask.getCreatorInfos() != null) {
+//											logInfo("Sleeping for 1 second while executing {}:{}, current task queue size: {}\n\t{}", this.currentTask.executable, this.currentTask.started, tasksQueue.size(),
+//													String.join("\n\t", this.currentTask.getCreatorInfos().stream().map(st -> st.toString()).collect(Collectors.toList())));
+//											Thread.sleep(1000);
+//										} else {
 											getMutex("executingFinishedWaiter").wait();
-										}
+//										}
 									} catch (InterruptedException exc) {
 										logWarn("Exception occurred", exc);
 									}
