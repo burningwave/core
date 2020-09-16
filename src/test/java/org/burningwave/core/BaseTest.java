4fe74bf43b3673ea9760bd24bb565d01f74cfeb7
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-09-16 14:23:56 +0200

    Reset tests

--- src/test/java/org/burningwave/core/BaseTest.java
+++ src/test/java/org/burningwave/core/BaseTest.java
@@ -16,12 +16,15 @@ import java.util.function.Supplier;
 
 import org.burningwave.core.assembler.ComponentContainer;
 import org.burningwave.core.assembler.ComponentSupplier;
+import org.junit.jupiter.api.MethodOrderer.Alphanumeric;
 import org.junit.jupiter.api.MethodOrderer.Random;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.api.function.ThrowingSupplier;
 
-@TestMethodOrder(Random.class)
+@SuppressWarnings("unused")
+//@TestMethodOrder(Random.class)
+//@TestMethodOrder(Alphanumeric.class)
 public class BaseTest implements Component {
 
 	Collection<ComponentSupplier> componentSuppliers = new CopyOnWriteArrayList<>();
