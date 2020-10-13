952d4f799ba9101756b648a9bdde0a159087c365
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-10-13 10:49:04 +0200

    Improved performance

--- src/main/java/org/burningwave/core/concurrent/Thread.java
+++ src/main/java/org/burningwave/core/concurrent/Thread.java
@@ -361,13 +361,13 @@ public class Thread extends java.lang.Thread implements ManagedLogger {
 								synchronized (this) {
 									runningThreads.remove(this);
 									executable = null;
+									if (!alive) {
+										continue;
+									}
 									setIndexedName();
 									poolableSleepingThreads.add(this);
 									synchronized (poolableSleepingThreads) {
 										poolableSleepingThreads.notifyAll();
-									}
-									if (!alive) {
-										continue;
 									}
 									wait();
 								}
