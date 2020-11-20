1031e0d81c4d6125015cd5f1c22a333242d04af4
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-11-20 17:35:29 +0100

    Updated example

--- src/test/java/org/burningwave/core/examples/member/MethodsHandler.java
+++ src/test/java/org/burningwave/core/examples/member/MethodsHandler.java
@@ -1,10 +1,13 @@
 package org.burningwave.core.examples.member;
 
+import static org.burningwave.core.assembler.StaticComponentContainer.Classes;
 import static org.burningwave.core.assembler.StaticComponentContainer.Methods;
 
 import java.lang.invoke.MethodHandle;
+import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 import java.security.ProtectionDomain;
+import java.util.Collection;
 
 import org.burningwave.core.classes.MethodCriteria;
 
@@ -22,23 +25,33 @@ public class MethodsHandler {
         //Invoking method by using MethodHandle
         Methods.invokeDirect(System.out, "println", number);
         
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		
         //Filtering and obtaining a MethodHandle reference
-        Methods.findFirstDirectHandle(
-        	MethodCriteria.byScanUpTo((cls) -> 
-				cls.getName().equals(ClassLoader.class.getName())
-			).name(
-				"defineClass"::equals
-			).and().parameterTypes(params -> 
-				params.length == 3
-			).and().parameterTypesAreAssignableFrom(
-				String.class, ByteBuffer.class, ProtectionDomain.class
-			).and().returnType((cls) -> 
-				cls.getName().equals(Class.class.getName())
-			), classLoader.getClass()
+        MethodHandle methodHandle = Methods.findFirstDirectHandle(
+            MethodCriteria.byScanUpTo((cls) ->
+            //We only analyze the ClassLoader class and not all of its hierarchy (default behavior)
+                cls.getName().equals(ClassLoader.class.getName())
+            ).name(
+                "defineClass"::equals
+            ).and().parameterTypes(params -> 
+                params.length == 3
+            ).and().parameterTypesAreAssignableFrom(
+                String.class, ByteBuffer.class, ProtectionDomain.class
+            ).and().returnType((cls) -> 
+                cls.getName().equals(Class.class.getName())
+            ), ClassLoader.class
+        );        
+        
+        //Filtering and obtaining all methods of ClassLoader class that have at least
+        //one input parameter of Class type
+        Collection<Method> methods = Methods.findAll(
+            MethodCriteria.byScanUpTo((cls) ->
+            	//We only analyze the ClassLoader class and not all of its hierarchy (default behavior)
+                cls.getName().equals(ClassLoader.class.getName())
+            ).parameter((params, idx) -> {
+                return Classes.isAssignableFrom(params[idx].getType(), Class.class);
+            }), ClassLoader.class
         );
-
+        methods.stream();
     }
     
     public static void main(String[] args) {
