60cfdb24f83e2d10b8a3cf44065b4f0d1b77cff9
Roberto Gentili <roberto.gentili.1980@gmail.com>
2020-11-21 10:52:13 +0100

    Added example

--- src/test/java/org/burningwave/core/examples/member/FieldsHandler.java
+++ src/test/java/org/burningwave/core/examples/member/FieldsHandler.java
@@ -4,7 +4,10 @@ import static org.burningwave.core.assembler.StaticComponentContainer.Fields;
 
 import java.lang.reflect.Field;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+
+import org.burningwave.core.classes.FieldCriteria;
 
 
 @SuppressWarnings("unused")
@@ -17,10 +20,29 @@ public class FieldsHandler {
         //Access by Reflection
         loadedClasses = Fields.get(classLoader, "classes");
         
-        //Get all fields of an object through memory address access
+        //Get all field values of an object through memory address access
         Map<Field, ?> values = Fields.getAllDirect(classLoader);
-        //Get all fields of an object through reflection access
+        //Get all field values of an object through reflection access
         values = Fields.getAll(classLoader);
+        
+        Object obj = new Object() {
+			volatile List<Object> objectValue;
+			volatile int intValue;
+			volatile long longValue;
+			volatile float floatValue;
+			volatile double doubleValue;
+			volatile boolean booleanValue;
+			volatile byte byteValue;
+			volatile char charValue;
+		};
+		
+		//Get all filtered field values of an object through memory address access
+		Fields.getAllDirect(
+			FieldCriteria.create().allThat(field -> {
+				return field.getType().isPrimitive();
+			}), 
+			obj
+		).values();
     }
     
     public static void main(String[] args) {
