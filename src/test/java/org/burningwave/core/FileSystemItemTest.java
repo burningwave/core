4e6660c84aebcae5ee4d143d50d80abfbd14a040
Roberto Gentili <roberto.gentili.1980@gmail.com>
2021-09-03 20:48:54 +0200

    Added test

--- src/test/java/org/burningwave/core/FileSystemItemTest.java
+++ src/test/java/org/burningwave/core/FileSystemItemTest.java
@@ -3,6 +3,9 @@ package org.burningwave.core;
 import static org.burningwave.core.assembler.StaticComponentContainer.ManagedLoggersRepository;
 
 import java.net.URL;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.burningwave.core.assembler.ComponentSupplier;
 import org.burningwave.core.io.FileSystemItem;
@@ -402,5 +405,20 @@ public class FileSystemItemTest extends BaseTest {
 			ManagedLoggersRepository.logDebug(getClass()::getName, url.toString());
 			return url;
 		});
+	}
+	
+	@Test
+	public void javaHomeTreeTest() {
+		testNotEmpty(() -> {
+				Set<String> files = ConcurrentHashMap.newKeySet();
+				FileSystemItem.ofPath(System.getProperty("java.home")).findInAllChildren(
+					FileSystemItem.Criteria.forAllFileThat(fileSystemItem -> {
+						files.add(fileSystemItem.getAbsolutePath());
+						return true;
+					})
+				);
+				return new TreeSet<>(files);
+			}, true
+		);
 	}
 }
