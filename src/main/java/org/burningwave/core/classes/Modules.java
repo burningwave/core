a74618bf660a7efdea9449dcd5c2ba8db8e2650c
Roberto Gentili <roberto.gentili.1980@gmail.com>
2021-09-20 10:18:23 +0200

    Improved functionality

--- src/main/java/org/burningwave/core/classes/Modules.java
+++ src/main/java/org/burningwave/core/classes/Modules.java
@@ -28,8 +28,10 @@
  */
 package org.burningwave.core.classes;
 
+
 import static org.burningwave.core.assembler.StaticComponentContainer.Fields;
 import static org.burningwave.core.assembler.StaticComponentContainer.Methods;
+import static org.burningwave.core.assembler.StaticComponentContainer.Strings;
 import static org.burningwave.core.assembler.StaticComponentContainer.Throwables;
 
 import java.util.HashMap;
@@ -37,19 +39,29 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.burningwave.core.function.TriConsumer;
+
 @SuppressWarnings("unchecked")
 public class Modules {
 	private Class<?> moduleClass;
-	private Class<?> moduleLayerClass;
+	private Set<?> allSet = new HashSet<>();
 	private Set<?> everyOneSet = new HashSet<>();
+	private Set<?> allUnnamedSet = new HashSet<>();
+	private Map<String, ?> nameToModule;
 	
-	private Modules() {
+	Modules() {
 		try {
 			moduleClass = Class.forName("java.lang.Module");
-			moduleLayerClass = Class.forName("java.lang.ModuleLayer");
+			Class<?> moduleLayerClass = Class.forName("java.lang.ModuleLayer");
+			Object moduleLayer = Methods.invokeStaticDirect(moduleLayerClass, "boot");
+			nameToModule = Fields.getDirect(moduleLayer, "nameToModule");
+			allSet = new HashSet<>();
+			allSet.add(Fields.getStaticDirect(moduleClass, "ALL_UNNAMED_MODULE"));
+			allSet.add(Fields.getStaticDirect(moduleClass, "EVERYONE_MODULE"));
 			everyOneSet = new HashSet<>();
-			everyOneSet.add(Fields.getStaticDirect(moduleClass, "ALL_UNNAMED_MODULE"));
 			everyOneSet.add(Fields.getStaticDirect(moduleClass, "EVERYONE_MODULE"));
+			allUnnamedSet = new HashSet<>();
+			allUnnamedSet.add(Fields.getStaticDirect(moduleClass, "ALL_UNNAMED_MODULE"));
 		} catch (Throwable exc) {
 			Throwables.throwException(exc);
 		}
@@ -61,11 +73,10 @@ public class Modules {
 	
 	public void exportAllToAll() {
 		try {
-			Object moduleLayer = Methods.invokeStaticDirect(moduleLayerClass, "boot");
-			((Set<Object>)Methods.invokeDirect(moduleLayer, "modules")).forEach(module -> {
+			nameToModule.forEach((name, module) -> {
 				((Set<String>)Methods.invokeDirect(module, "getPackages")).forEach(pkgName -> {
-					addTo("exportedPackages", module, pkgName);
-					addTo("openPackages", module, pkgName);
+					exportToAll("exportedPackages", module, pkgName);
+					exportToAll("openPackages", module, pkgName);
 				});
 			});
 		} catch (Throwable exc) {
@@ -73,18 +84,105 @@ public class Modules {
 		}
 	}
 	
-	void addTo(String fieldName, Object module, String pkgName) {
+	
+	public void exportToAllUnnamed(String name) {
+		exportTo(name, this::exportToAllUnnamed);
+	}
+	
+	
+	public void exportToAll(String name) {
+		exportTo(name, this::exportToAll);
+	}
+	
+	public void export(String from, String to) {
+		try {
+			Object moduleFrom = checkAndGetModule(from);
+			Object moduleTo = checkAndGetModule(to);
+			((Set<String>)Methods.invokeDirect(moduleFrom, "getPackages")).forEach(pkgName -> {
+				export("exportedPackages", moduleFrom, pkgName, moduleTo);
+				export("exportedPackages", moduleFrom, pkgName, moduleTo);
+			});
+		} catch (Throwable exc) {
+			Throwables.throwException(exc);
+		}
+	}
+	
+
+	Object checkAndGetModule(String from) {
+		Object module = nameToModule.get(from);
+		if (module == null) {
+			throw new NotFoundException(Strings.compile("Module named name {} not found", from));
+		}
+		return module;
+	}
+	
+	public void exportTo(String name, TriConsumer<String, Object, String> exporter) {
+		try {
+			Object module = checkAndGetModule(name);
+			((Set<String>)Methods.invokeDirect(module, "getPackages")).forEach(pkgName -> {
+				exporter.accept("exportedPackages", module, pkgName);
+				exporter.accept("openPackages", module, pkgName);
+			});
+		} catch (Throwable exc) {
+			Throwables.throwException(exc);
+		}
+	}
+	
+	
+	void exportToAll(String fieldName, Object module, String pkgName) {
 		Map<String, Set<?>> pckgForModule = Fields.getDirect(module, fieldName);
 		if (pckgForModule == null) {
 			pckgForModule = new HashMap<>();
 			Fields.setDirect(module, fieldName, pckgForModule);
 		}
-		pckgForModule.put(pkgName, everyOneSet);
+		pckgForModule.put(pkgName, allSet);
 		if (fieldName.startsWith("exported")) {	
-			Methods.invokeStaticDirect(moduleClass, "addExportsToAllUnnamed0", module, pkgName);
 			Methods.invokeStaticDirect(moduleClass, "addExportsToAll0", module, pkgName);
 		}
 	}
-
 	
+	
+	void exportToAllUnnamed(String fieldName, Object module, String pkgName) {
+		Map<String, Set<?>> pckgForModule = Fields.getDirect(module, fieldName);
+		if (pckgForModule == null) {
+			pckgForModule = new HashMap<>();
+			Fields.setDirect(module, fieldName, pckgForModule);
+		}
+		pckgForModule.put(pkgName, allUnnamedSet);
+		if (fieldName.startsWith("exported")) {	
+			Methods.invokeStaticDirect(moduleClass, "addExportsToAllUnnamed0", module, pkgName);
+		}
+	}
+	
+	void export(String fieldName, Object moduleFrom, String pkgName, Object moduleTo) {
+		Map<String, Set<?>> pckgForModule = Fields.getDirect(moduleFrom, fieldName);
+		if (pckgForModule == null) {
+			pckgForModule = new HashMap<>();
+			Fields.setDirect(moduleFrom, fieldName, pckgForModule);
+		}
+		Set<Object> moduleSet = (Set<Object>)pckgForModule.get(pkgName);
+		if (!(moduleSet instanceof HashSet)) {
+			if (moduleSet != null) {
+				moduleSet = new HashSet<>(moduleSet);
+			} else {
+				moduleSet = new HashSet<>();
+			}			
+			pckgForModule.put(pkgName, moduleSet);
+		}
+		moduleSet.add(moduleTo);
+		if (fieldName.startsWith("exported")) {	
+			Methods.invokeStaticDirect(moduleClass, "addExports0", moduleFrom, pkgName, moduleTo);
+		}		
+	}
+
+
+	public static class NotFoundException extends RuntimeException {
+
+		private static final long serialVersionUID = 3095842376538548262L;
+		
+		public NotFoundException(String message) {
+	        super(message);
+	    }
+		
+	}
 }
