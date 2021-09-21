23e7e7a25a72278f6b5cb80c494c398cd8b7f127
Roberto Gentili <roberto.gentili.1980@gmail.com>
2021-09-21 15:56:17 +0200

    Added test

--- src/test/java/org/burningwave/core/ModulesTest.java
+++ src/test/java/org/burningwave/core/ModulesTest.java
@@ -4,20 +4,27 @@ import static org.burningwave.core.assembler.StaticComponentContainer.JVMInfo;
 import static org.burningwave.core.assembler.StaticComponentContainer.Modules;
 import static org.burningwave.core.assembler.StaticComponentContainer.Resources;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SuppressWarnings("unchecked")
 public class ModulesTest extends BaseTest {
 
 	
 	@Test
-	public void testExportAllToAll() {
+	@Order(1)
+	public void exportPackageToAllUnnamedTest() {
 		testDoesNotThrow(() -> {
 			if (JVMInfo.getVersion() > 8) {
-				Modules.exportAllToAll();
+				Modules.exportPackageToAllUnnamed ("java.base", "java.net");
 			    Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
 			    method.setAccessible(true);
 			    ClassLoader classLoader = new URLClassLoader(new URL[] {}, null);
@@ -30,6 +37,26 @@ public class ModulesTest extends BaseTest {
 			    	Resources.getClassPath(Modules.getClass()).getURL()
 			    );
 			    classLoader.loadClass(ModulesTest.class.getName());
+			}
+		});
+	}
+	
+	
+	@Test
+	@Order(10)
+	public void exportAllToAllTest() {
+		testDoesNotThrow(() -> {
+			if (JVMInfo.getVersion() > 8) {
+				Modules.exportAllToAll();
+	    		Class<?> bootClassLoaderClass = Class.forName("jdk.internal.loader.ClassLoaders$BootClassLoader");
+				Constructor<? extends ClassLoader> constructor = (Constructor<? extends ClassLoader>)
+	    			Class.forName("jdk.internal.loader.ClassLoaders$PlatformClassLoader").getDeclaredConstructor(bootClassLoaderClass);
+	    		constructor.setAccessible(true);
+	    		Class<?> classLoadersClass = Class.forName("jdk.internal.loader.ClassLoaders");
+	    		Method bootClassLoaderRetriever = classLoadersClass.getDeclaredMethod("bootLoader");
+	    		bootClassLoaderRetriever.setAccessible(true);
+	    		ClassLoader newBuiltinclassLoader = constructor.newInstance(bootClassLoaderRetriever.invoke(classLoadersClass));
+	    		System.out.println(newBuiltinclassLoader + " instantiated");
 			}
 		});
 	}
