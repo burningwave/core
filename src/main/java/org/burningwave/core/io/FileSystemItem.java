7b0b649196f030bd8e418e5c14dd710e7752810a
Roberto Gentili <roberto.gentili.1980@gmail.com>
2021-10-15 14:12:01 +0200

    Testing

--- src/main/java/org/burningwave/core/io/FileSystemItem.java
+++ src/main/java/org/burningwave/core/io/FileSystemItem.java
@@ -332,7 +332,7 @@ public class FileSystemItem implements Comparable<FileSystemItem> {
 	}
 
 	public FileSystemItem findFirstInAllChildren(FileSystemItem.Criteria filter) {
-		return findIn(this::getAllChildren0, filter, true, ConcurrentHashMap::newKeySet).stream().findFirst().orElseGet(() -> null);
+		return findFirstInChildren(this::getAllChildren0, filter);
 	}
 
 	public FileSystemItem findFirstInChildren() {
@@ -340,8 +340,37 @@ public class FileSystemItem implements Comparable<FileSystemItem> {
 	}
 
 	public FileSystemItem findFirstInChildren(FileSystemItem.Criteria filter) {
-		return findIn(this::getChildren0, filter, true, ConcurrentHashMap::newKeySet).stream().findFirst().orElseGet(() -> null);
+		return findFirstInChildren(this::getChildren0, filter);
 	}
+
+	private FileSystemItem findFirstInChildren(Supplier<Set<FileSystemItem>> childrenSupplier,
+			FileSystemItem.Criteria filter) {
+		Predicate<FileSystemItem[]> filterPredicate = filter.getPredicateOrTruePredicateIfPredicateIsNull();
+		FileSystemItem[] childAndThis = new FileSystemItem[] { null, this };
+		for (FileSystemItem fileSystemItem : childrenSupplier.get()) {
+			childAndThis[0] = fileSystemItem;
+			if (filterPredicate.test(childAndThis)) {
+				return fileSystemItem;
+			}
+		}
+		return null;
+	}
+	
+//	public FileSystemItem findFirstInAllChildren() {
+//		return findFirstInAllChildren(FileSystemItem.Criteria.create());
+//	}
+//
+//	public FileSystemItem findFirstInAllChildren(FileSystemItem.Criteria filter) {
+//		return findIn(this::getAllChildren0, filter, true, ConcurrentHashMap::newKeySet).stream().findFirst().orElseGet(() -> null);
+//	}
+//
+//	public FileSystemItem findFirstInChildren() {
+//		return findFirstInAllChildren(FileSystemItem.Criteria.create());
+//	}
+//
+//	public FileSystemItem findFirstInChildren(FileSystemItem.Criteria filter) {
+//		return findIn(this::getChildren0, filter, true, ConcurrentHashMap::newKeySet).stream().findFirst().orElseGet(() -> null);
+//	}
 
 	public String getAbsolutePath() {
 		return absolutePath.getKey();
