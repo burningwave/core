1539ec56bc190914e639e1eabd1e7c068f41de48
Roberto Gentili <roberto.gentili.1980@gmail.com>
2021-11-10 15:55:53 +0100

    Improved architecture

--- src/main/java/org/burningwave/core/concurrent/Thread.java
+++ src/main/java/org/burningwave/core/concurrent/Thread.java
@@ -552,17 +552,8 @@ public class Thread extends java.lang.Thread implements ManagedLogger {
 				}
 				if (poolableSleepingThreads[i] == thread) {
 					synchronized (thread) {
-						if (thread.getState() == Thread.State.WAITING) {
-							poolableSleepingThreads[i] = null;
-							return thread;
-						} else {
-							ManagedLoggersRepository.logWarn(
-								getClass()::getName,
-								"Poolable thread {} is not in a waiting state: \n{}",
-								thread.hashCode(),
-								Strings.from(thread.getStackTrace(), 0)
-							);
-						}
+						poolableSleepingThreads[i] = null;
+						return thread;
 					}
 				}
 			}
