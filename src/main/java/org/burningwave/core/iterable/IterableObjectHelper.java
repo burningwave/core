f2a027bf98ba734ab2541d8fc2ddfd53a44dd2b9
Roberto Gentili <roberto.gentili.1980@gmail.com>
2021-11-17 15:20:41 +0100

    Modified comment

--- src/main/java/org/burningwave/core/iterable/IterableObjectHelper.java
+++ src/main/java/org/burningwave/core/iterable/IterableObjectHelper.java
@@ -78,7 +78,7 @@ public interface IterableObjectHelper {
 			defaultValues.put(Key.PARELLEL_ITERATION_APPLICABILITY_DEFAULT_MINIMUM_COLLECTION_SIZE, 2);
 			
 			String defaultValuesSeparator = (String)defaultValues.get(Key.DEFAULT_VALUES_SEPERATOR);
-			//The semicolons in this value value will be replaced by the method StaticComponentContainer.adjustConfigurationValues
+			//The semicolons in this value will be replaced by the method StaticComponentContainer.adjustConfigurationValues
 			defaultValues.put(
 				Key.PARELLEL_ITERATION_APPLICABILITY_OUTPUT_COLLECTION_ENABLED_TYPES,
 				ConcurrentHashMap.class.getName() + "$CollectionView" + defaultValuesSeparator +
