edcdaf0309b2e5d62e6c162894aacd7bdb9fcee5
Roberto Gentili <roberto.gentili.1980@gmail.com>
2021-11-29 13:39:04 +0100

    Modified test

--- src/test/java/org/burningwave/core/BackgroundExecutorTest.java
+++ src/test/java/org/burningwave/core/BackgroundExecutorTest.java
@@ -6,7 +6,9 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
+import org.burningwave.core.concurrent.QueuedTasksExecutor;
 import org.junit.jupiter.api.Test;
 
 public class BackgroundExecutorTest extends BaseTest {
@@ -26,11 +28,31 @@ public class BackgroundExecutorTest extends BaseTest {
 		AtomicBoolean executed = new AtomicBoolean();
 		assertTrue(			
 			BackgroundExecutor.createTask(() -> {
-				Thread.sleep(5000);		
+				Thread.sleep(10000);		
 				executed.set(true);
 			}).runOnlyOnce(
 				UUID.randomUUID().toString(), executed::get
 			).submit().waitForFinish(2500).kill().isAborted()
+		);
+	}
+	
+	@Test
+	public void killTestThree() {
+		AtomicBoolean executed = new AtomicBoolean();
+		AtomicReference<QueuedTasksExecutor.Task> childTask = new AtomicReference<>();
+		QueuedTasksExecutor.Task mainTask = BackgroundExecutor.createTask(() -> {
+			childTask.set(BackgroundExecutor.createTask(() -> {
+				while(true) {}
+			}).runOnlyOnce(
+				UUID.randomUUID().toString(), executed::get
+			).submit());
+			Thread.sleep(30000);
+			executed.set(true);
+		}).runOnlyOnce(
+			UUID.randomUUID().toString(), executed::get
+		).submit().waitForFinish(2500).kill();
+		assertTrue(			
+			mainTask.wasKilled() && childTask.get().wasKilled()
 		);
 	}
 
