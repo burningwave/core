b68a73b37defd0b0db448e49642b08382fc525e7
Roberto Gentili <roberto.gentili.1980@gmail.com>
2021-12-01 16:29:26 +0100

    Disabled test

--- src/test/java/org/burningwave/core/RepeatedComponentContainerTest.java
+++ src/test/java/org/burningwave/core/RepeatedComponentContainerTest.java
@@ -2,11 +2,19 @@ package org.burningwave.core;
 
 public class RepeatedComponentContainerTest extends ComponentContainerTest {
 	
+//	@org.junit.jupiter.api.Test
+//	@org.junit.jupiter.api.Order(6)
+//	@Override
+//	public void putPropertyFour() {
+//		super.putPropertyFour();
+//	}
+	
+	
 	//@org.junit.jupiter.api.Test
-	//@org.junit.jupiter.api.Order(6)
+	//@org.junit.jupiter.api.Order(5)
 	@Override
-	public void putPropertyFour() {
-		super.putPropertyFour();
+	public void resetAndCloseTest() {
+		
 	}
 
 }
