2cf8f8d6396eec65e874a4d9328074dee6e5b788
Roberto Gentili <roberto.gentili.1980@gmail.com>
2021-12-06 10:45:42 +0100

    Re-added reInit test

--- src/test/java/org/burningwave/core/ComponentContainerTest.java
+++ src/test/java/org/burningwave/core/ComponentContainerTest.java
@@ -48,8 +48,8 @@ public class ComponentContainerTest extends BaseTest {
 	}
 	
 
-//	@Test
-//	@Order(3)
+	@Test
+	@Order(3)
 	public void reInit() {
 		testDoesNotThrow(() -> {
 			getComponentSupplier().reInit();
