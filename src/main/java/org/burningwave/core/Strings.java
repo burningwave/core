f15489ab820c867226121dc147f85fd6c4a9a6ba
Roberto Gentili <roberto.gentili.1980@gmail.com>
2022-07-06 13:55:06 +0200

    Improved performance

--- src/main/java/org/burningwave/core/Strings.java
+++ src/main/java/org/burningwave/core/Strings.java
@@ -300,7 +300,12 @@ public class Strings {
 			if (path.chars().filter(ch -> ch == '/').count() == 0 || path.equals("/")) {
 				return path;
 			}
-			return clean(java.nio.file.Paths.get(path).normalize().toAbsolutePath().toString());
+			return java.nio.file.Paths.get(path).normalize().toAbsolutePath().toString();
+		}
+
+		public boolean isRoot(String path) {
+			path = clean(path);
+			return path.chars().filter(ch -> ch == '/').count() == 0 || path.equals("/");
 		}
 
 		public String getExtension(String path) {
