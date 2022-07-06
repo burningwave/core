8e2932763f751685179b8d98dde4dfe0e8995e6f
Roberto Gentili <roberto.gentili.1980@gmail.com>
2022-07-06 18:10:01 +0200

    Added new tests

--- src/test/java/org/burningwave/core/FileSystemItemTest.java
+++ src/test/java/org/burningwave/core/FileSystemItemTest.java
@@ -198,6 +198,14 @@ public class FileSystemItemTest extends BaseTest {
 	}
 
 	@Test
+	public void getChildrenOfRootTestOne() {
+		ComponentSupplier componentSupplier = getComponentSupplier();
+		testNotEmpty(() -> componentSupplier.getPathHelper().getResource(
+			"/../../src/test/external-resources/libs-for-test.zip/ESC-Lib.ear/APP-INF/lib/jaxb-xjc-2.1.7.jar/1.0"
+		).getRoot().getChildren(), true);
+	}
+
+	@Test
 	public void readTestFifteen() {
 		ComponentSupplier componentSupplier = getComponentSupplier();
 		String basePath = componentSupplier.getPathHelper().getPath((path) -> path.endsWith("target/test-classes"));
