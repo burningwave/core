9e3d4d981d3e414b5b5dc3ab4248b0f5d14c4fa5
Roberto Gentili <roberto.gentili.1980@gmail.com>
2022-07-06 13:46:22 +0200

    Bug fix

--- src/main/java/org/burningwave/core/Strings.java
+++ src/main/java/org/burningwave/core/Strings.java
@@ -296,6 +296,10 @@ public class Strings {
 		}
 
 		public String toNormalizedCleanedAbsolutePath(String path) {
+			path = clean(path);
+			if (path.chars().filter(ch -> ch == '/').count() == 0 || path.equals("/")) {
+				return path;
+			}
 			return clean(java.nio.file.Paths.get(path).normalize().toAbsolutePath().toString());
 		}
 
