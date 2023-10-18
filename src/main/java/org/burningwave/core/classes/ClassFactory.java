f29b89ba02c84d59f3e2179798e1d0597dfa9542
Roberto Gentili <roberto.gentili.1980@gmail.com>
2023-10-18 17:15:22 +0200

    License update

--- src/main/java/org/burningwave/core/classes/ClassFactory.java
+++ src/main/java/org/burningwave/core/classes/ClassFactory.java
@@ -9,7 +9,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2019-2022 Roberto Gentili
+ * Copyright (c) 2019-2023 Roberto Gentili
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without
